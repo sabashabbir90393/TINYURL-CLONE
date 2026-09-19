d6de88fb5e55d4015d738bd2c740b4e66555a166
nosheenshabbirahmad435-arch <nosheenshabbirahmad435@gmail.com>
2026-09-19 11:08:38 +0500

    updated

--- auth.js
+++ auth.js
@@ -186,3 +186,17 @@ function logoutUser() {
 
   window.location.href = "/landing.html";
 }
+function togglePassword(inputId, button) {
+  const input = document.getElementById(inputId);
+  const icon = button.querySelector("i");
+
+  if (input.type === "password") {
+    input.type = "text";
+    icon.classList.remove("fa-eye");
+    icon.classList.add("fa-eye-slash");
+  } else {
+    input.type = "password";
+    icon.classList.remove("fa-eye-slash");
+    icon.classList.add("fa-eye");
+  }
+}
